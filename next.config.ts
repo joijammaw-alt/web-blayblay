15bbbdd8967f115ff6ec3423b48c848d54acff60
joijammaw-alt <joijammaw@gmail.com>
2026-05-15 17:31:06 +0700

    แก้จ้าาา

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   basePath: "/blayblay", // เพิ่มบรรทัดนี้เพื่อให้แอปรับรู้ว่าตัวเองอยู่ที่ /blayblay
   trailingSlash: true,    // ช่วยให้การจัดการ Path ผ่าน Proxy เสถียรขึ้น
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
