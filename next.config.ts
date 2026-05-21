d5d140f51ffde014d9399c72f0a49a5432f8e2f0
joijammaw-alt <joijammaw@gmail.com>
2026-05-21 11:54:37 +0700

    edit next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  basePath: '/home',
+  basePath: '/blayblay',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
